d7d66d824ce37a2df269e9d60186de07a58628b0
Jeremy Wharton <jeremy.e.wharton@gmail.com>
2021-08-28 18:46:11 -0500

    Initial commit

---
+++ gen.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+//go:generate go run gen.go
+
+var consoleServices []string = []string{
+	"auth",
+	"payments",
+}
+
+type Endpoint struct {
+	Path     string
+	Method   string
+	FuncName string
+	Request  map[string]interface{}
+	Response map[string]interface{}
+}
+
+type ApiDef map[string][]Endpoint
+
+func main() {
+	data, err := ioutil.ReadFile("api.json")
+	check(err)
+
+	var api ApiDef
+	check(json.Unmarshal(data, &api))
+
+	genInfos := []struct {
+		TemplatePath string
+		OutputPath   string
+	}{
+		{TemplatePath: "templates/consoleweb.tmpl", OutputPath: "satellite/console/consoleweb/api.go"},
+		{TemplatePath: "templates/consoleapi.tmpl", OutputPath: "satellite/console/consoleweb/consoleapi/api.go"},
+		{TemplatePath: "templates/web.tmpl", OutputPath: "web/satellite/src/api/api.ts"},
+	}
+
+	for _, genInfo := range genInfos {
+		tmpl, err := template.New(filepath.Base(genInfo.TemplatePath)).Funcs(template.FuncMap{
+			"sub": func(a, b int) int {
+				return a - b
+			},
+			"strtitle": func(s string) string {
+				return strings.Title(s)
+			},
+			"substr": func(s string, start, end int) string {
+				return s[start:end]
+			},
+			"tolower": func(s string) string {
+				return strings.ToLower(s)
+			},
+		}).ParseFiles(genInfo.TemplatePath)
+		check(err)
+
+		check(os.MkdirAll(filepath.Dir(genInfo.OutputPath), os.ModePerm))
+
+		check(os.Truncate(genInfo.OutputPath, 0))
+
+		out, err := os.OpenFile(genInfo.OutputPath, os.O_CREATE, 0644)
+		check(err)
+
+		check(tmpl.Execute(out, struct {
+			Services []string
+			ApiDef   ApiDef
+		}{
+			Services: consoleServices,
+			ApiDef:   api,
+		}))
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (e Endpoint) GetGoFuncName() (name string) {
+	name = e.Method
+	pathParts := strings.Split(strings.ReplaceAll(e.Path[1:], "/", "-"), "-")
+	for i, part := range pathParts {
+		pathParts[i] = strings.Title(strings.ToLower(part))
+	}
+	return name + strings.Join(pathParts, "_")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xaresys/storj-api-gen
+
+go 1.16
