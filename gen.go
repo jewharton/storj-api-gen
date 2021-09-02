8a52ab9e78145bceede59608192319256fb643af
Jeremy Wharton <jeremy.e.wharton@gmail.com>
2021-09-02 09:50:53 -0500

    Fix error: invalid file permission

--- gen.go
+++ gen.go
@@ -67,7 +67,7 @@ func main() {
 
 		check(os.MkdirAll(filepath.Dir(genInfo.OutputPath), os.ModePerm))
 
-		out, err := os.OpenFile(genInfo.OutputPath, os.O_CREATE|os.O_WRONLY, os.ModeAppend)
+		out, err := os.OpenFile(genInfo.OutputPath, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 		check(err)
 
 		check(out.Truncate(0))
